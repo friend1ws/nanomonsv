3f1dbf291d65639f03872220254eceb5276814a7
friend1ws <friend1ws@gmail.com>
2020-07-27 13:46:05 +0000

    modified .travis.yml

--- tests/check_download.py
+++ tests/check_download.py
@@ -6,7 +6,7 @@ def check_download(url, output_path):
     
     import requests
 
-    if not os.path.exists(os.path.dirname(output_path)):
+    if os.path.dirname(output_path) != "" and not os.path.exists(os.path.dirname(output_path)):
        os.makedirs(os.path.dirname(output_path))
 
     if not os.path.exists(output_path):
