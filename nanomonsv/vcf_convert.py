d3b750fb9128a56bf0fc447d92865e1462b1ac86
friend1ws <friend1ws@gmail.com>
2021-03-07 06:37:20 +0000

    several modifications on vcf_convert.py

--- nanomonsv/vcf_convert.py
+++ nanomonsv/vcf_convert.py
@@ -21,25 +21,36 @@ def genomesv2vcf_convert(result_file, output_vcf, reference):
         header = header + '\n' + f"##contig=<ID={tchr},length={tlen}>"
 
     header = header + '\n' + \
+            '##FILTER=<ID=Duplicate_with_close_SV,Description="When multiple SVs that share breakpoints in close proximity are detected, all but one SVs are filtered.">\n'\
+            '##FILTER=<ID=Duplicate_with_insertion,Description="Breakend SVs that are inferred to be the same as any of detected insertions">\n'\
+            '##FILTER=<ID=Duplicate_with_close_insertion,Description="When multiple insertions in close proximity are detected, all but one insertions are filtered.">\n'\
+            '##FILTER=<ID=SV_with_decoy,Description="SVs involving decoy contigs">\n'\
+            '##FILTER=<ID=Too_small_size,Description="Insertions whose size is below the threshould (currently 100bp)">\n'\
+            '##FILTER=<ID=Too_low_VAF,Description="SVs whose variant allele frequencies are inferred to be low">\n'\
             '##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">\n'\
-            '##INFO=<ID=SVLEN,Number=.,Type=Integer,Description="Difference in length between REF and ALT alleles">\n'\
+            '##INFO=<ID=SVLEN,Number=1,Type=Integer,Description="Difference in length between REF and ALT alleles">\n'\
             '##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the variant described in this record">\n'\
-            '##INFO=<ID=MATEID,Number=.,Type=String,Description="ID of mate breakend">\n'\
-            '##INFO=<ID=SVINSLEN,Number=.,Type=Integer,Description="Length of insertion">\n'\
-            '##INFO=<ID=SVINSSEQ,Number=.,Type=String,Description="Sequence of insertion">\n'\
+            '##INFO=<ID=MATEID,Number=1,Type=String,Description="ID of mate breakend">\n'\
+            '##INFO=<ID=SVINSLEN,Number=1,Type=Integer,Description="Length of insertion">\n'\
+            '##INFO=<ID=SVINSSEQ,Number=1,Type=String,Description="Sequence of insertion">\n'\
             '##ALT=<ID=DEL,Description="Deletion">\n'\
             '##ALT=<ID=INS,Description="Insertion">\n'\
             '##ALT=<ID=DUP,Description="Duplication">\n'\
-            '##FORMAT=<ID=TR,Number=.,Type=Integer,Description="The number of reads around the breakpoints">\n'\
-            '##FORMAT=<ID=VR,Number=.,Type=Integer,Description="The number of variant supporting reads determined in the validation realignment step">\n'\
-            '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tTUMOR\tCONTROL'
+            '##FORMAT=<ID=TR,Number=1,Type=Integer,Description="The number of reads around the breakpoints">\n'\
+            '##FORMAT=<ID=VR,Number=1,Type=Integer,Description="The number of variant supporting reads determined in the validation realignment step">'
 
     with open(result_file, 'r') as hin, open(output_vcf, 'w') as hout:
 
-        print(header,  file = hout)
         dreader = csv.DictReader(hin, delimiter = '\t')
         fieldname_list = dreader.fieldnames
         is_control = True if "Checked_Read_Num_Control" in fieldname_list and "Supporting_Read_Num_Control" in fieldname_list else False
+
+        if is_control:
+            header = header + '\n' + "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tTUMOR\tCONTROL"
+        else:
+            header = header + '\n' + "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tTUMOR"
+
+        print(header,  file = hout)
 
         for F in dreader:
 
@@ -63,9 +74,10 @@ def genomesv2vcf_convert(result_file, output_vcf, reference):
 
                 tpos = int(F["Pos_1"])
                 tref = ref_tb.fetch(tchrom, tpos - 1, tpos)
+                if tref == '' or tref is None: continue
                 tsvlen = int(F["Pos_2"]) - int(F["Pos_1"]) - 1
                 tend = int(F["Pos_2"]) - 1
-
+    
                 # Deletion
                 if tsvlen > tsvinslen:
                     talt = "<DEL>"
@@ -76,10 +88,12 @@ def genomesv2vcf_convert(result_file, output_vcf, reference):
                         tinfo = tinfo + f";SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
 
                 # Insertion
-                else:
+                elif tsvlen > 0:
                     talt = "<INS>"
                     tinfo = f"END={tend};SVTYPE=INS;SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
 
+                else:
+                    continue
                 print(f"{tchrom}\t{tpos}\t{tid}\t{tref}\t{talt}\t{tqual}\t{tfilter}\t{tinfo}\t{tformat_sample}", file = hout)
 
             # Duplication
@@ -87,6 +101,7 @@ def genomesv2vcf_convert(result_file, output_vcf, reference):
 
                 tpos = int(F["Pos_1"]) - 1
                 tref = ref_tb.fetch(tchrom, tpos - 1, tpos)
+                if tref == '' or tref is None: continue
                 talt = "<DUP>"
                 tend = int(F["Pos_2"]) 
                 tsvlen = int(F["Pos_2"]) - int(F["Pos_1"]) + 1
@@ -99,33 +114,41 @@ def genomesv2vcf_convert(result_file, output_vcf, reference):
             # Breakend 
             else:
                 tchrom1 = F["Chr_1"]
-                tpos = int(F["Pos_1"])
-                tref = ref_tb.fetch(tchrom1, tpos - 1, tpos)
-                tbracket = ']' if F["Dir_2"] == '+' else '['
-                if F["Dir_1"] == '+':
-                    talt = f'{tref}{tsvinsseq}{tbracket}{F["Chr_2"]}:{F["Pos_2"]}{tbracket}'
-                else:
-                    talt = f'{tbracket}{F["Chr_2"]}:{F["Pos_2"]}{tbracket}{tsvinsseq}{tref}' 
-
-                tinfo = f"SVTYPE=BND;MATEID={tid}_0"
-                if tsvinslen != 0: tinfo = tinfo + f";SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
-
-                print(f"{tchrom1}\t{tpos}\t{tid}_0\t{tref}\t{talt}\t{tqual}\t{tfilter}\t{tinfo}\t{tformat_sample}", file = hout)
+                tpos1 = int(F["Pos_1"])
+                tref1 = ref_tb.fetch(tchrom1, tpos1 - 1, tpos1)
+                if tref1 == '' or tref1 is None: continue
 
                 tchrom2 = F["Chr_2"]
-                tpos = int(F["Pos_2"])
-                tref = ref_tb.fetch(tchrom2, tpos - 1, tpos)
+                tpos2 = int(F["Pos_2"])
+                tref2 = ref_tb.fetch(tchrom2, tpos2 - 1, tpos2)
+                if tref2 == '' or tref2 is None: continue
+
+                tbracket = ']' if F["Dir_2"] == '+' else '['
+                if F["Dir_1"] == '+':
+                    talt1 = f'{tref1}{tsvinsseq}{tbracket}{tchrom2}:{tpos2}{tbracket}'
+                else:
+                    talt1 = f'{tbracket}{tchrom2}:{tpos2}{tbracket}{tsvinsseq}{tref2}' 
+
+                tinfo1 = f"SVTYPE=BND;MATEID={tid}_1"
+                if tsvinslen != 0: tinfo1 = tinfo1 + f";SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
+
+                print(f"{tchrom1}\t{tpos1}\t{tid}_0\t{tref1}\t{talt1}\t{tqual}\t{tfilter}\t{tinfo1}\t{tformat_sample}", file = hout)
+
+                # tchrom2 = F["Chr_2"]
+                # tpos = int(F["Pos_2"])
+                # tref = ref_tb.fetch(tchrom2, tpos - 1, tpos)
+                # if tref == '' or tref is None: continue
                 tbracket = ']' if F["Dir_1"] == '+' else '['
                 tsvinsseq = reverse_complement(tsvinsseq)
                 if F["Dir_2"] == '+':
-                    talt = f'{tref}{tsvinsseq}{tbracket}{F["Chr_1"]}:{F["Pos_1"]}{tbracket}'
+                    talt2 = f'{tref2}{tsvinsseq}{tbracket}{tchrom1}:{tpos1}{tbracket}'
                 else:
-                    talt = f'{tbracket}{F["Chr_1"]}:{F["Pos_1"]}{tbracket}{tsvinsseq}{tref}'
+                    talt2 = f'{tbracket}{tchrom1}:{tpos1}{tbracket}{tsvinsseq}{tref2}'
 
-                tinfo = f"SVTYPE=BND;MATEID={tid}_1"
-                if tsvinslen != 0: tinfo = tinfo + f";SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
+                tinfo2 = f"SVTYPE=BND;MATEID={tid}_0"
+                if tsvinslen != 0: tinfo2 = tinfo2 + f";SVINSLEN={tsvinslen};SVINSSEQ={tsvinsseq}"
 
-                print(f"{tchrom2}\t{tpos}\t{tid}_1\t{tref}\t{talt}\t{tqual}\t{tfilter}\t{tinfo}\t{tformat_sample}", file = hout)
+                print(f"{tchrom2}\t{tpos2}\t{tid}_1\t{tref2}\t{talt2}\t{tqual}\t{tfilter}\t{tinfo2}\t{tformat_sample}", file = hout)
 
 
 if __name__ == "__main__":
