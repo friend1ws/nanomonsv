5c5289522e21fb16bf54df03ead0b251f5a506f2
aokad <aokada@ncc.go.jp>
2022-06-29 18:31:26 +0900

    remove temporary files

--- nanomonsv/run.py
+++ nanomonsv/run.py
@@ -355,6 +355,7 @@ def get_main(args):
     if not args.debug:
         for i in range(parallel_num):
             os.remove(args.tumor_prefix + ".support_read_seq.%d.txt" % (i))
+            os.remove(args.tumor_prefix + ".support_read_seq.sbnd.%d.txt" % (i))
             os.remove(args.tumor_prefix + ".consensus_seq.%d.txt" % (i))
             os.remove(args.tumor_prefix + ".consensus_seq.sbnd.%d.txt" % (i))
             os.remove(args.tumor_prefix + ".refined_bp.%d.txt" % (i))
@@ -368,8 +369,6 @@ def get_main(args):
                 os.remove(args.tumor_prefix + ".realignment.control.sread_count.sbnd.%d.txt" % (i))
                 os.remove(args.tumor_prefix + ".realignment.control.sread_info.%d.txt" % (i))
                 os.remove(args.tumor_prefix + ".realignment.control.sread_info.sbnd.%d.txt" % (i))
-            if not args.single_bnd:
-                os.remove(args.tumor_prefix + ".support_read_seq.sbnd.%d.txt" % (i))
 
     logger.info("Final processing") 
     control_sread_count_file = args.tumor_prefix + ".realignment.control.sread_count.txt" if args.control_bam is not None else None
@@ -395,6 +394,8 @@ def get_main(args):
         os.remove(args.tumor_prefix + ".insertion.sorted.clustered.bedpe")
         os.remove(args.tumor_prefix + ".deletion.sorted.clustered.bedpe")
         os.remove(args.tumor_prefix + ".singlebreakend.sorted.clustered.bed")
+        os.remove(args.tumor_prefix + ".support_read_seq.txt")
+        os.remove(args.tumor_prefix + ".support_read_seq.sbnd.txt")
         #os.remove(args.tumor_prefix + ".consensus_seq.txt")
         #os.remove(args.tumor_prefix + ".consensus_seq.sbnd.txt")
         #os.remove(args.tumor_prefix + ".refined_bp.txt")
@@ -411,7 +412,7 @@ def get_main(args):
 
         if not args.single_bnd:
             os.remove(args.tumor_prefix + ".nanomonsv.sbnd.result.txt")   
-            #os.remove(args.tumor_prefix + ".support_read_seq.sbnd.txt")
+
 
 def validate_main(args):
    
