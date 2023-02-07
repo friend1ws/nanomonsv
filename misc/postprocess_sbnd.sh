9ac762206bd3ca4a44bd05d98550679c9ccf2cef
friend1ws <friend1ws@gmail.com>
2023-02-07 06:25:00 +0000

    modified postprocess_sbnd.sh

--- misc/postprocess_sbnd.sh
+++ misc/postprocess_sbnd.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
 NANOMONSV_PREFIX=$1
+REFERENCE_GENOME=$2
+SIMPLE_REPEAT_BED=$3
 
-python3 subscript_postprocess_sbnd/integrate_sbnd.py ${NANOMONSV_PREFIX} ~/environment/data/reference/GRCh38.d1.vd1.fa
+python3 subscript_postprocess_sbnd/integrate_sbnd.py ${NANOMONSV_PREFIX} ${REFERENCE_GENOME}
 
-python3 subscript_postprocess_sbnd/add_simple_repeat.py ${NANOMONSV_PREFIX}.nanomonsv.proc.result.txt ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt.tmp simpleRepeat.bed.gz   
+python3 subscript_postprocess_sbnd/add_simple_repeat.py ${NANOMONSV_PREFIX}.nanomonsv.proc.result.txt ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt.tmp ${SIMPLE_REPEAT_BED}
 
-nanomonsv subscript_postprocess_sbnd/insert_classify ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt.tmp ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt ~/environment/data/reference/GRCh38.d1.vd1.fa --genome_id hg38
+nanomonsv subscript_postprocess_sbnd/insert_classify ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt.tmp ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt ${REFERENCE_GENOME} --genome_id hg38
 
-python3 subscript_postprocess_sbnd/add_annot_sbnd.py ${NANOMONSV_PREFIX}.nanomonsv.sbnd.proc.result.txt simpleRepeat.bed.gz > ${NANOMONSV_PREFIX}.nanomonsv.sbnd.annot.proc.result.txt 
+python3 subscript_postprocess_sbnd/add_annot_sbnd.py ${NANOMONSV_PREFIX}.nanomonsv.sbnd.proc.result.txt ${SIMPLE_REPEAT_BED} > ${NANOMONSV_PREFIX}.nanomonsv.sbnd.annot.proc.result.txt 
 
 Rscript subscript_postprocess_sbnd/plot_sbnd_vis.R ${NANOMONSV_PREFIX} ${NANOMONSV_PREFIX}.nanomonsv.sbnd_vis
 
