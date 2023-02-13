060f73f95723848cf18fbcd068721d672cf2a8d6
friend1ws <friend1ws@gmail.com>
2023-02-13 08:37:10 +0000

    modified postprocess_sbnd.sh

--- misc/postprocess_sbnd.sh
+++ misc/postprocess_sbnd.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+set -e
 
 NANOMONSV_PREFIX=$1
 REFERENCE_GENOME=$2
@@ -10,7 +11,7 @@ python3 subscript_postprocess_sbnd/add_simple_repeat.py ${NANOMONSV_PREFIX}.nano
 
 nanomonsv insert_classify ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt.tmp ${NANOMONSV_PREFIX}.nanomonsv.annot.proc.result.txt ${REFERENCE_GENOME} --genome_id hg38
 
-python3 subscript_postprocess_sbnd/add_annot_sbnd.py ${NANOMONSV_PREFIX}.nanomonsv.sbnd.proc.result.txt ${SIMPLE_REPEAT_BED} > ${NANOMONSV_PREFIX}.nanomonsv.sbnd.annot.proc.result.txt 
+python3 subscript_postprocess_sbnd/add_simple_repeat_sbnd.py ${NANOMONSV_PREFIX}.nanomonsv.sbnd.proc.result.txt ${SIMPLE_REPEAT_BED} > ${NANOMONSV_PREFIX}.nanomonsv.sbnd.annot.proc.result.txt 
 
 Rscript subscript_postprocess_sbnd/plot_sbnd_vis.R ${NANOMONSV_PREFIX} ${NANOMONSV_PREFIX}.nanomonsv.sbnd_vis
 
